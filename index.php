adf5fc273f54004a32bf0c7e8d66d1f6fb6705f2
racossj12 <oscarssj1@gmail.com>
2021-10-05 19:42:37 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+
+require_once 'persona.php';
+
+
+$persona1 = new persona() ;
+$persona1->calcularIMC('ELionor',1.80,90);
---
+++ persona.php
@@ -0,0 +1,27 @@
+<?php 
+class persona {
+    public $nombre ;
+    public $altura;
+    public $peso ; 
+    
+    public function calcularIMC($nombre,$altura,$peso)
+    {   $IMC = ( $peso/ ($altura*$altura));
+        if ($IMC == 18.5){
+            echo ($nombre. " esta en "."delgadez o bajo
+            peso");
+        } elseif ($IMC <24.9 && $IMC > 18.5) {
+            echo ($nombre. " esta en peso "."normal");
+        }
+     elseif ($IMC <29.9 && $IMC > 25) {
+        echo ($nombre." esta en "."sobrepeso");
+    }elseif ($IMC > 30) {
+        echo ($nombre." esta en "."obesidad");
+    }
+    elseif($IMC == 0 ){
+        echo(" que pendejo3 ");
+    }
+    else{
+        echo(" que pendejo ".$IMC);
+    }
+}
+}
