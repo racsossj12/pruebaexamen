eb30eb1718251664428a939c88547eb74dd0e380
racossj12 <oscarssj1@gmail.com>
2021-10-05 20:03:29 -0700

    cambio para mi

--- persona.php
+++ persona.php
@@ -9,12 +9,12 @@ class persona {
         if ($IMC == 18.5){
             echo ($nombre. " esta en "."delgadez o bajo
             peso");
-        } elseif ($IMC <24.9 && $IMC > 18.5) {
+        } elseif ($IMC <=24.9 && $IMC >= 18.5) {
             echo ($nombre. " esta en peso "."normal");
         }
-     elseif ($IMC <29.9 && $IMC > 25) {
+     elseif ($IMC <=29.9 && $IMC >= 25) {
         echo ($nombre." esta en "."sobrepeso");
-    }elseif ($IMC > 30) {
+    }elseif ($IMC >= 30) {
         echo ($nombre." esta en "."obesidad");
     }
     elseif($IMC == 0 ){
